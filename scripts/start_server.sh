be455da35679db50839c88875bb9aaa37d197040
Seungsu Yoo <zenorate@gmail.com>
2025-11-13 13:45:03 +0900

    add nvm configurations

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+
+export NVM_DIR="/home/ec2-user/.nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+	. "$NVM_DIR/nvm.sh"
+fi
+
+nvm use 24
+
 cd /var/www/myapp
 
 # PM2 설치 (전역으로, 없을 경우)
