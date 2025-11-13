089f316ea964f1f836428e3fb4cb4335dd7b14dd
Seungsu Yoo <zenorate@gmail.com>
2025-11-13 13:47:28 +0900

    add nvm configurations to bash scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+
+export NVM_DIR="/home/ec2-user/.nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+	. "$NVM_DIR/nvm.sh"
+fi
+
+nvm use 24
+
 cd /var/www/myapp
 
 # Node.js 의존성 설치
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+export NVM_DIR="/home/ec2-user/.nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+	. "$NVM_DIR/nvm.sh"
+fi
+
+nvm use 24
 
 # PM2 프로세스 중지 (있을 경우)
 if command -v pm2 &> /dev/null; then
